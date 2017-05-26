3becd6e73522f09e61c69dd71a21210bed5579fc
Angela Nicholas <angelaknicholas@gmail.com>
2017-05-26 15:19:01 -0700

    update demo, add tooltip back

--- demo/demo.js
+++ demo/demo.js
@@ -1,10 +1,49 @@
 import React from "react";
+import filesize from "filesize";
 import { VictorySunburst } from "../src/index";
+import buildHierarchy from "./buildHierarchy";
+import stats from "json!./stats.json";
+
+const data = buildHierarchy(stats.modules);
+const fontSize = 16;
+const lineHeight = fontSize * 1.5;
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.handleArcHover = this.handleArcHover.bind(this);
+  }
+
+  handleArcHover(activeNode) {
+    this.setState({ activeNode });
+  }
+
   render() {
+    const { activeNode } = this.state;
+
     return (
-      <VictorySunburst />
+      <svg height={700} width={700}>
+        <VictorySunburst
+          data={data}
+          onArcHover={this.handleArcHover}
+        />
+        <g transform={`translate(${350},${350})`}>
+          {activeNode ? (
+            <text fill="black" fontSize={fontSize} textAnchor="middle" fontFamily="Helvetica">
+              <tspan x="0" dy={-lineHeight}>
+                {activeNode.data.name}
+              </tspan>
+              <tspan x="0" dy={lineHeight}>
+                {`${(activeNode.data.size / data.size * 100).toFixed(2)}%`}
+              </tspan>
+              <tspan x="0" dy={lineHeight}>
+                {filesize(activeNode.data.size)}
+              </tspan>
+            </text>
+          ) : null}
+        </g>
+      </svg>
     );
   }
 }
