7b5700e098df24ce69c011384912d3378f5943f4
Angela Nicholas <angelaknicholas@gmail.com>
2017-06-19 16:50:04 -0700

    remove demo tooltip

--- demo/demo.js
+++ demo/demo.js
@@ -1,45 +1,7 @@
 /* eslint-disable no-magic-numbers */
 import React from "react";
-import filesize from "filesize";
 import { VictorySunburst } from "../src/index";
-import flare from "./flare.js";
-
-// import { buildHierarchy } from "./utils";
-// const data = buildHierarchy(stats.modules);
-
-const fontSize = 14;
-const lineHeight = fontSize * 1.4;
-const size = 400;
-const tooltipHeight = fontSize * 6;
-const tooltipOffset = 10;
-
-const svgStyles = {
-  height: size,
-  overflow: "visible",
-  viewBox: `0 0 ${size} ${size}`,
-  width: size
-};
-const rectStyles = {
-  fill: "white",
-  height: tooltipHeight,
-  opacity: 0.8,
-  rx: 4,
-  ry: 4,
-  stroke: "gray",
-  strokeOpacity: 0.5,
-  strokeWidth: 0.5,
-  width: 210
-};
-const textStyles = {
-  fill: "black",
-  fontFamily: "Helvetica",
-  fontSize
-};
-const tspanStyles = {
-  alignmentBaseline: "middle",
-  x: 18,
-  y: tooltipHeight / 2
-};
+// import flare from "./flare.js";
 
 export default class App extends React.Component {
   constructor() {
@@ -63,41 +25,17 @@ export default class App extends React.Component {
   }
 
   render() {
-    const { activeNodeIndex, clientX, clientY } = this.state;
-    const translate = `translate(${clientX + tooltipOffset},${clientY + tooltipOffset})`;
-
     return (
-      <svg {...svgStyles}>
-        <VictorySunburst
-          activeNodeIndex={activeNodeIndex}
-          events={[{
-            target: "data",
-            eventHandlers: {
-              onMouseOver: this.handleDataMouseOver,
-              onMouseOut: this.handleDataMouseOut
-            }
-          }]}
-          height={size}
-          width={size}
-        />
-      </svg>
+      <VictorySunburst
+        activeNodeIndex={this.state.activeNodeIndex}
+        events={[{
+          target: "data",
+          eventHandlers: {
+            onMouseOver: this.handleDataMouseOver,
+            onMouseOut: this.handleDataMouseOut
+          }
+        }]}
+      />
     );
   }
 }
-
-// {activeNode ? (
-//   <g transform={translate}>
-//     <rect {...rectStyles} />
-//     <text {...textStyles}>
-//       <tspan dy={-lineHeight} fontWeight="bold" {...tspanStyles}>
-//         {activeNode.data.name}
-//       </tspan>
-//       <tspan dy={0} {...tspanStyles}>
-//         {`${(activeNode.data.size / activeNode.parent.data.size * 100).toFixed(2)}%`}
-//       </tspan>
-//       <tspan dy={lineHeight} {...tspanStyles}>
-//         {filesize(activeNode.data.size)}
-//       </tspan>
-//     </text>
-//   </g>
-// ) : null}
