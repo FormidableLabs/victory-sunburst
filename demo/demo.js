204c469d6db8720a4e430d601e7cabd0743b1ade
Angela Nicholas <angelaknicholas@gmail.com>
2017-06-20 17:24:29 -0700

    add moving tooltip sunburst example

--- demo/demo.js
+++ demo/demo.js
@@ -11,32 +11,58 @@ export default class App extends React.Component {
     this.handleDataMouseOut = this.handleDataMouseOut.bind(this);
   }
 
-  handleDataMouseOver({ clientX, clientY }, { index }) {
+  handleDataMouseOver(ev, props, index, parent) { // eslint-disable-line max-params
     const { activeNodeIndex } = this.state;
-    const newState = { clientX, clientY };
+    let newState = { clientX: ev.clientX, clientY: ev.clientY };
     if (!activeNodeIndex) {
-      newState.activeNodeIndex = index;
+      newState = {
+        ...newState,
+        activeName: parent.props.name,
+        activeNodeIndex: parseInt(index)
+      };
     }
     this.setState(newState);
   }
 
   handleDataMouseOut() {
-    this.setState({ activeNodeIndex: null });
+    this.setState({ activeName: null, activeNodeIndex: null });
   }
 
   render() {
+    const { activeName, activeNodeIndex, clientX, clientY } = this.state;
     return (
-      <VictorySunburst
-        alwaysDisplayLabel
-        activeNodeIndex={this.state.activeNodeIndex}
-        events={[{
-          target: "data",
-          eventHandlers: {
-            onMouseOver: this.handleDataMouseOver,
-            onMouseOut: this.handleDataMouseOut
-          }
-        }]}
-      />
+      <div>
+        <VictorySunburst
+          name="fixedTooltip"
+          alwaysDisplayLabel
+          activeNodeIndex={activeName === "fixedTooltip" ? activeNodeIndex : 0}
+          events={[{
+            target: "data",
+            eventHandlers: {
+              onMouseOver: this.handleDataMouseOver,
+              onMouseOut: this.handleDataMouseOut
+            }
+          }]}
+        />
+        <VictorySunburst
+          name="movingTooltip"
+          activeNodeIndex={activeName === "movingTooltip" ? activeNodeIndex : 0}
+          colorScale="red"
+          events={[{
+            target: "data",
+            eventHandlers: {
+              onMouseOver: this.handleDataMouseOver,
+              onMouseOut: this.handleDataMouseOut
+            }
+          }]}
+          labelProps={{
+            x: clientX - 400,
+            y: clientY,
+            dx: 10,
+            dy: 10
+          }}
+        />
+      </div>
     );
   }
 }
