dc252dda813ef364d9f2b43333219884eb4b8f6a
Angela Nicholas <angelaknicholas@gmail.com>
2017-07-06 20:35:48 -0700

    moving hover tooltip demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,14 +1,45 @@
+/* global window */
 /* eslint-disable no-magic-numbers */
 import React from "react";
 import { VictoryTooltip } from "victory-core";
 import { VictorySunburst } from "../src/index";
 // import flare from "./flare.js";
 
+const size = 400;
+
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.handleDataMouseMove = this.handleDataMouseMove.bind(this);
+    this.handleDataMouseOut = this.handleDataMouseOut.bind(this);
+  }
+
+  componentDidMount() {
+    const { top, left } = this.sunburst.getBoundingClientRect();
+    this.x = left;
+    this.y = top;
+  }
+
+  handleDataMouseMove(ev, props) {
+    const { datum: { data } } = props;
+    this.setState({
+      activeText: `${data.name}: ${data.size}`,
+      clientX: ev.clientX,
+      clientY: ev.clientY + window.scrollY
+    });
+  }
+
+  handleDataMouseOut() {
+    this.setState({ clientX: null, clientY: null });
+  }
+
   render() {
+    const { activeText, clientX, clientY } = this.state;
+
     return (
       <div>
-        <VictorySunburst />
+        <VictorySunburst/>
         <VictorySunburst
           name="fixedLabel"
           colorScale="blue"
@@ -23,28 +54,66 @@ export default class App extends React.Component {
         <VictorySunburst
           name="fixedTooltip"
           colorScale="red"
-          labelComponent={<VictoryTooltip active={(d) => !!d.parent} height={30} width={40} />}
+          labelComponent={
+            <VictoryTooltip
+              active={(d) => !!d.parent}
+              height={30}
+              style={{ fill: "black" }}
+              width={40}
+            />
+          }
           labels={(d) => d.data.size}
-          style={{
-            labels: { fill: "black" }
-          }}
         />
         <VictorySunburst
-          name="hoverTooltip"
+          name="fixedHoverTooltip"
           colorScale="green"
           labelComponent={
             <VictoryTooltip
-              active={(d) => !d.parent} orientation="bottom"
-              pointerLength={0} height={40} width={60} x={0} y={-20}
+              active={(d) => !d.parent}
+              height={40}
+              orientation="bottom"
+              pointerLength={0}
+              style={{ fill: "black" }}
+              width={60}
+              x={0}
+              y={-20}
             />
           }
-          labels={({ data: { name, size } }, totalSize) => {
-            return `${name}: ${size}\n(${Math.round(size / totalSize * 100)}%)`;
-          }}
-          style={{
-            labels: { fill: "black" }
+          labels={(datum, totalSize) => {
+            const { data } = datum;
+            return `${data.name}: ${data.size}\n(${Math.round(data.size / totalSize * 100)}%)`;
           }}
         />
+        <svg
+          height={size}
+          ref={(el) => { this.sunburst = el; }}
+          style={{ overflow: "visible" }}
+          viewBox={`0 0 ${size} ${size}`}
+          width={size}
+        >
+          <VictorySunburst
+            name="movingHoverTooltip"
+            colorScale="qualitative"
+            events={[{
+              target: "data",
+              eventHandlers: {
+                onMouseMove: this.handleDataMouseMove,
+                onMouseOut: this.handleDataMouseOut
+              }
+            }]}
+            standalone={false}
+            style={{ labels: { display: "none" } }}
+          />
+          <VictoryTooltip
+            active={!!clientX && !!clientY}
+            pointerLength={0}
+            renderInPortal={false}
+            style={{ fill: "black" }}
+            text={activeText}
+            x={clientX - this.x}
+            y={clientY - this.y}
+          />
+        </svg>
       </div>
     );
   }
